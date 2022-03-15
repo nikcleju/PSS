c6b08a15cadd1c715f34d78a7eb04270b8c9a91f
Nicolae Cleju <nikcleju@gmail.com>
2022-03-15 11:58:36 +0200

    Update

--- Labs/Jupyter/keepalive.py
+++ Labs/Jupyter/keepalive.py
@@ -5,8 +5,8 @@ keepAlive = True
 
 def threadHandler():
     print('thread started')
-    sleepfor = 290   # seconds
-    sleepmax = 30  # 30 * 290 seconds = 30 * 5 min = 150 mins
+    sleepfor = 180   # seconds
+    sleepmax = 50  # 30 * 290 seconds = 30 * 5 min = 150 mins
     sleepcount = 0
     while keepAlive and sleepcount < sleepmax:
         print(f"(Keep alive {sleepcount+1}/{sleepmax}, sleep for {sleepfor} seconds)")
